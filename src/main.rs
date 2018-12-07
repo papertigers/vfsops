334ed368cab57203fc0c152f5aedbc1abe131a01
Mike Zeller <mike@mikezeller.net>
2018-12-07 03:12:56 +0000

    Add proper CLI support

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+extern crate clap;
 extern crate kstat;
 
+use clap::{Arg, App};
 use kstat::kstat_named::KstatNamedData;
 use kstat::{KstatData, KstatReader};
 use std::collections::HashMap;
@@ -17,7 +19,10 @@ macro_rules! write_output(
     } }
 );
 
-fn print_header() {
+fn print_header(hide: bool) {
+    if hide {
+        return;
+    }
     write_output!("zone", "10ms_ops", "100ms_ops", "1s_ops", "10s_ops");
 }
 
@@ -63,13 +68,18 @@ fn get_stats(data: &HashMap<String, KstatNamedData>) -> Stats {
     }
 }
 
-fn print_stats(curr: &ZoneHash, old: &Option<ZoneHash>) {
+fn print_stats(curr: &ZoneHash, old: &Option<ZoneHash>, zone: &Option<String>, all: &bool) {
     let mut keys: Vec<_> = curr.keys().collect();
     keys.sort_by_key(|k| k.abs()); // Is there a better way to just sort without abs
 
     for key in keys {
         let stat = &curr[key];
         let zonename = &read_string(stat.data.get("zonename").unwrap());
+        if zone.is_some() {
+            if zone.as_ref().unwrap() != *zonename {
+                continue;
+            }
+        }
         let len = if zonename.len() >= 8 { 8 } else { zonename.len() };
         let zonename = &read_string(stat.data.get("zonename").unwrap())[0..len];
 
@@ -92,34 +102,108 @@ fn print_stats(curr: &ZoneHash, old: &Option<ZoneHash>) {
         let one_second = stats.one_second - old_stats.one_second;
         let ten_second = stats.ten_second - old_stats.ten_second;
 
+        if !all {
+            if ten_ms == 0 && one_hundred_ms == 0 && one_second == 0 && ten_second == 0 {
+                continue;
+            }
+        }
+
         write_output!(zonename, ten_ms, one_hundred_ms, one_second, ten_second);
     }
 }
 
 fn main() {
-    let mut counter = 0;
+    let about = r#"
+The vfsops utility reports vfs operation outliers. A count of the number of operations are grouped
+into 10ms, 100ms, 1s, and 10s buckets. The first line of output represents a zone's outliers since
+boot, while sequential lines show how many operations have occured during the INTERVAL. By default
+vfsops will only output zones that have a non zero value for all buckets.
+        "#;
+    let matches = App::new("vfsops")
+        .version("0.1.0")
+        .author("Mike Zeller <mike@mikezeller.net")
+        .about("Report VFS op outliers by bucket group")
+        .long_about(about)
+        .arg(Arg::with_name("H")
+            .short("H")
+            .help("Don't print the header"))
+        .arg(Arg::with_name("z")
+            .short("z")
+            .long("zone")
+            .help("Print data for a specific zonename")
+            .value_name("ZONE"))
+        .arg(Arg::with_name("Z")
+            .short("Z")
+            .help("Print zones with no activity"))
+        .arg(Arg::with_name("INTERVAL")
+            .help("Print results per inverval rather than per second")
+            .required(true)
+            .index(1))
+        .arg(Arg::with_name("COUNT")
+            .help("Print for n times and exit")
+            .required(false)
+            .index(2))
+        .get_matches();
+
+    let hide_header = matches.is_present("H");
+    let show_all_zones = matches.is_present("Z");
+    let zone_filter = match matches.value_of("z") {
+        Some(zone) => Some(String::from(zone)),
+        None => None,
+    };
+    let interval = match matches.value_of("INTERVAL") {
+        None => 1,
+        Some(val) => match val.parse::<i32>() {
+            Ok(i) => i,
+            Err(_) => {
+                println!("please provide a valid INTERVAL");
+                ::std::process::exit(1);
+            }
+        }
+    };
+
+    let count = match matches.value_of("COUNT") {
+        None => None,
+        Some(val) => match val.parse::<i32>() {
+            Ok(i) => Some(i),
+            Err(_) => {
+                println!("please provide a valid COUNT");
+                ::std::process::exit(1);
+            }
+        }
+    };
+
+    let mut header_interval = 0;
+    let mut nloops = 0;
     let mut old: Option<ZoneHash> = None;
     let reader =
         KstatReader::new(None, None, None, Some("zone_vfs")).expect("failed to create reader");
-    let _interval = Some(1);
 
-    print_header();
+    print_header(hide_header);
     loop {
+        // there must be a better way to do this?
+        if count.is_some() {
+            if nloops >= *count.as_ref().unwrap() {
+                break;
+            }
+        }
         let stats = reader.read().expect("failed to read kstats");
         let curr = zone_hashmap(stats);
 
-        if counter > 5 {
-            print_header();
-            counter = 0;
+        if header_interval > 5 {
+            print_header(hide_header);
+            header_interval = 0;
         }
-        print_stats(&curr, &old);
+        print_stats(&curr, &old, &zone_filter, & show_all_zones);
         let _ = ::std::io::stderr().flush();
 
         // move curr -> old
         old = Some(curr);
-        counter += 1;
+        header_interval += 1;
+        if count.is_some() {
+            nloops += 1;
+        }
 
-        // hardcoded sleep for now
-        thread::sleep(time::Duration::from_secs(5));
+        thread::sleep(time::Duration::from_secs(interval as u64));
     }
 }
